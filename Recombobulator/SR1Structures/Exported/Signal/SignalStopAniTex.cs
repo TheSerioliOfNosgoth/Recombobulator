8e808f86be17be56c754b61f59880d1639bb994f
afradley <andrew.fradley.83@gmail.com>
2021-08-26 15:52:26 -0300

    Yet more signals from Feb 16.

---
+++ Recombobulator/SR1Structures/Exported/Signal/SignalStopAniTex.cs
@@ -0,0 +1,21 @@
+﻿namespace Recombobulator.SR1Structures
+{
+    class SignalStopAnitex : SignalData
+    {
+        SR1_Primative<int> misc = new SR1_Primative<int>();
+
+        protected override void ReadMembers(SR1_Reader reader, SR1_Structure parent)
+        {
+            misc.Read(reader, this, "misc");
+        }
+
+        protected override void ReadReferences(SR1_Reader reader, SR1_Structure parent)
+        {
+        }
+
+        public override void WriteMembers(SR1_Writer writer)
+        {
+            misc.Write(writer);
+        }
+    }
+}
