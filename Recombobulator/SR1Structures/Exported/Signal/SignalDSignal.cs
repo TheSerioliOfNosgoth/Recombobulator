dfc9d6fcc66979e717cdf99601620a9e583478e4
afradley <andrew.fradley.83@gmail.com>
2021-08-26 15:42:49 -0300

    Add more signal types from Feb 16.

---
+++ Recombobulator/SR1Structures/Exported/Signal/SignalDSignal.cs
@@ -0,0 +1,24 @@
+﻿namespace Recombobulator.SR1Structures
+{
+    class SignalDSignal : SignalData
+    {
+        SR1_Pointer<Intro> intro = new SR1_Pointer<Intro>();
+        SR1_Pointer<object> data = new SR1_Pointer<object>();
+
+        protected override void ReadMembers(SR1_Reader reader, SR1_Structure parent)
+        {
+            intro.Read(reader, this, "intro");
+            data.Read(reader, this, "data");
+        }
+
+        protected override void ReadReferences(SR1_Reader reader, SR1_Structure parent)
+        {
+        }
+
+        public override void WriteMembers(SR1_Writer writer)
+        {
+            intro.Write(writer);
+            data.Write(writer);
+        }
+    }
+}
