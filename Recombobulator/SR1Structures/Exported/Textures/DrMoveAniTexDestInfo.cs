ea7e59f407c9ed8bcfe42f03de11ca5f1f3501fa
AesirHod <andrew.fradley.83@gmail.com>
2024-02-10 20:00:30 +0000

    Added support for animated textures in retail PC builds.

--- Recombobulator/SR1Structures/Exported/Textures/DrMoveAniTexDestInfo.cs
+++ Recombobulator/SR1Structures/Exported/Textures/DrMoveAniTexDestInfo.cs
@@ -13,10 +13,16 @@ namespace Recombobulator.SR1Structures
 		SR1_Primative<short> clutDstY = new SR1_Primative<short>();
 		SR1_Primative<short> clutW = new SR1_Primative<short>();
 		SR1_Primative<short> clutH = new SR1_Primative<short>();
+		SR1_Primative<short> tPageDst = new SR1_Primative<short>();
+		SR1_Primative<short> pad0 = new SR1_Primative<short>();
+		SR1_Primative<short> pad1 = new SR1_Primative<short>();
+		SR1_Primative<short> pad2 = new SR1_Primative<short>();
 		SR1_Primative<short> pixCurrentX = new SR1_Primative<short>();
 		SR1_Primative<short> pixCurrentY = new SR1_Primative<short>();
 		SR1_Primative<short> clutCurrentX = new SR1_Primative<short>();
 		SR1_Primative<short> clutCurrentY = new SR1_Primative<short>();
+		SR1_Primative<short> tPageCurrent = new SR1_Primative<short>();
+		SR1_Primative<short> pad3 = new SR1_Primative<short>();
 		SR1_Primative<int> numFrames = new SR1_Primative<int>();
 		SR1_Primative<int> speed = new SR1_Primative<int>();
 		SR1_StructureArray<DrMoveAniTexSrcInfo> frame = new SR1_StructureArray<DrMoveAniTexSrcInfo>(0);
@@ -27,14 +33,26 @@ namespace Recombobulator.SR1Structures
 			pixDstY.Read(reader, this, "pixDstY");
 			pixW.Read(reader, this, "pixW");
 			pixH.Read(reader, this, "pixH");
-			clutDstX.Read(reader, this, "clutDstX");
-			clutDstY.Read(reader, this, "clutDstY");
-			clutW.Read(reader, this, "clutW");
-			clutH.Read(reader, this, "clutH");
+
+			clutDstX.Read(reader, this, "clutDstX", SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutDstY.Read(reader, this, "clutDstY", SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutW.Read(reader, this, "clutW", SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutH.Read(reader, this, "clutH", SR1_File.Version.First, SR1_File.Version.Retail_PC);
+
+			tPageDst.Read(reader, this, "tPageDst", SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad0.Read(reader, this, "pad0", SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad1.Read(reader, this, "pad1", SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad2.Read(reader, this, "pad2", SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+
 			pixCurrentX.Read(reader, this, "pixCurrentX");
 			pixCurrentY.Read(reader, this, "pixCurrentY");
-			clutCurrentX.Read(reader, this, "clutCurrentX");
-			clutCurrentY.Read(reader, this, "clutCurrentY");
+
+			clutCurrentX.Read(reader, this, "clutCurrentX", SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutCurrentY.Read(reader, this, "clutCurrentY", SR1_File.Version.First, SR1_File.Version.Retail_PC);
+
+			tPageCurrent.Read(reader, this, "tPageCurrent", SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad3.Read(reader, this, "pad3", SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+
 			numFrames.Read(reader, this, "numFrames");
 			speed.Read(reader, this, "speed");
 
@@ -52,14 +70,26 @@ namespace Recombobulator.SR1Structures
 			pixDstY.Write(writer);
 			pixW.Write(writer);
 			pixH.Write(writer);
-			clutDstX.Write(writer);
-			clutDstY.Write(writer);
-			clutW.Write(writer);
-			clutH.Write(writer);
+
+			clutDstX.Write(writer, SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutDstY.Write(writer, SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutW.Write(writer, SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutH.Write(writer, SR1_File.Version.First, SR1_File.Version.Retail_PC);
+
+			tPageDst.Write(writer, SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad0.Write(writer, SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad1.Write(writer, SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad2.Write(writer, SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+
 			pixCurrentX.Write(writer);
 			pixCurrentY.Write(writer);
-			clutCurrentX.Write(writer);
-			clutCurrentY.Write(writer);
+
+			clutCurrentX.Write(writer, SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutCurrentY.Write(writer, SR1_File.Version.First, SR1_File.Version.Retail_PC);
+
+			tPageCurrent.Write(writer, SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad3.Write(writer, SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+
 			numFrames.Write(writer);
 			speed.Write(writer);
 			frame.Write(writer);
--- Recombobulator/SR1Structures/Exported/Textures/DrMoveAniTexSrcInfo.cs
+++ Recombobulator/SR1Structures/Exported/Textures/DrMoveAniTexSrcInfo.cs
@@ -9,13 +9,17 @@ namespace Recombobulator.SR1Structures
 		SR1_Primative<short> pixSrcY = new SR1_Primative<short>();
 		SR1_Primative<short> clutSrcX = new SR1_Primative<short>();
 		SR1_Primative<short> clutSrcY = new SR1_Primative<short>();
+		SR1_Primative<short> tPageSrc = new SR1_Primative<short>();
+		SR1_Primative<short> pad = new SR1_Primative<short>();
 
 		protected override void ReadMembers(SR1_Reader reader, SR1_Structure parent)
 		{
 			pixSrcX.Read(reader, this, "pixSrcX");
 			pixSrcY.Read(reader, this, "pixSrcY");
-			clutSrcX.Read(reader, this, "clutSrcX");
-			clutSrcY.Read(reader, this, "clutSrcY");
+			clutSrcX.Read(reader, this, "clutSrcX", SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutSrcY.Read(reader, this, "clutSrcY", SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			tPageSrc.Read(reader, this, "tPageSrc", SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad.Read(reader, this, "pad", SR1_File.Version.Retail_PC, SR1_File.Version.Next);
 		}
 
 		protected override void ReadReferences(SR1_Reader reader, SR1_Structure parent)
@@ -26,8 +30,10 @@ namespace Recombobulator.SR1Structures
 		{
 			pixSrcX.Write(writer);
 			pixSrcY.Write(writer);
-			clutSrcX.Write(writer);
-			clutSrcY.Write(writer);
+			clutSrcX.Write(writer, SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			clutSrcY.Write(writer, SR1_File.Version.First, SR1_File.Version.Retail_PC);
+			tPageSrc.Write(writer, SR1_File.Version.Retail_PC, SR1_File.Version.Next);
+			pad.Write(writer, SR1_File.Version.Retail_PC, SR1_File.Version.Next);
 		}
 	}
 }
