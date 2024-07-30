b3acff0ce7f0999f045f35ad7a64c32c10801a43
AesirHod <andrew.fradley.83@gmail.com>
2024-07-30 22:28:18 +0100

    Updated MorphColor feilds to handle Proto1.

--- Recombobulator/SR1Structures/Exported/MorphColor.cs
+++ Recombobulator/SR1Structures/Exported/MorphColor.cs
@@ -5,11 +5,25 @@ namespace Recombobulator.SR1Structures
 {
 	class MorphColor : SR1_Structure
 	{
+		SR1_Primative<byte> r0 = new SR1_Primative<byte>();
+		SR1_Primative<byte> g0 = new SR1_Primative<byte>();
+		SR1_Primative<byte> b0 = new SR1_Primative<byte>();
+		SR1_Primative<byte> r1 = new SR1_Primative<byte>();
+		SR1_Primative<byte> g1 = new SR1_Primative<byte>();
+		SR1_Primative<byte> b1 = new SR1_Primative<byte>();
+		SR1_Primative<short> vindex = new SR1_Primative<short>();
 		SR1_Primative<short> morphColor15 = new SR1_Primative<short>();
 
 		protected override void ReadMembers(SR1_Reader reader, SR1_Structure parent)
 		{
-			morphColor15.Read(reader, this, "morphColor15");
+			r0.Read(reader, this, "r0", SR1_File.Version.First, SR1_File.Version.Jan23);
+			g0.Read(reader, this, "g0", SR1_File.Version.First, SR1_File.Version.Jan23);
+			b0.Read(reader, this, "b0", SR1_File.Version.First, SR1_File.Version.Jan23);
+			r1.Read(reader, this, "r1", SR1_File.Version.First, SR1_File.Version.Jan23);
+			g1.Read(reader, this, "g1", SR1_File.Version.First, SR1_File.Version.Jan23);
+			b1.Read(reader, this, "b1", SR1_File.Version.First, SR1_File.Version.Jan23);
+			vindex.Read(reader, this, "vindex", SR1_File.Version.First, SR1_File.Version.Jan23);
+			morphColor15.Read(reader, this, "morphColor15", SR1_File.Version.Jan23, SR1_File.Version.Next);
 		}
 
 		protected override void ReadReferences(SR1_Reader reader, SR1_Structure parent)
@@ -18,7 +32,14 @@ namespace Recombobulator.SR1Structures
 
 		public override void WriteMembers(SR1_Writer writer)
 		{
-			morphColor15.Write(writer);
+			r0.Write(writer, SR1_File.Version.First, SR1_File.Version.Jan23);
+			g0.Write(writer, SR1_File.Version.First, SR1_File.Version.Jan23);
+			b0.Write(writer, SR1_File.Version.First, SR1_File.Version.Jan23);
+			r1.Write(writer, SR1_File.Version.First, SR1_File.Version.Jan23);
+			g1.Write(writer, SR1_File.Version.First, SR1_File.Version.Jan23);
+			b1.Write(writer, SR1_File.Version.First, SR1_File.Version.Jan23);
+			vindex.Write(writer, SR1_File.Version.First, SR1_File.Version.Jan23);
+			morphColor15.Write(writer, SR1_File.Version.Jan23, SR1_File.Version.Next);
 		}
 	}
 }
