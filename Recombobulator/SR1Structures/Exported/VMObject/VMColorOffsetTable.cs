2fdf68e2ec3e6c339844e125b8b7b633222ac999
AesirHod <andrew.fradley.83@gmail.com>
2024-09-13 23:44:29 +0100

    Fixed VMOs for Proto1 and found a helpful optimization in the process.

--- Recombobulator/SR1Structures/Exported/VMObject/VMColorOffsetTable.cs
+++ Recombobulator/SR1Structures/Exported/VMObject/VMColorOffsetTable.cs
@@ -8,9 +8,18 @@ namespace Recombobulator.SR1Structures
 		SR1_Primative<int> numVMOffsets = new SR1_Primative<int>();
 		SR1_StructureArray<VMColorOffset> offsets = new SR1_StructureArray<VMColorOffset>(0);
 
+		public VMColorOffsetTable()
+		{
+		}
+
+		public VMColorOffsetTable(int numOffsets)
+		{
+			numVMOffsets.Value = numOffsets;
+		}
+
 		protected override void ReadMembers(SR1_Reader reader, SR1_Structure parent)
 		{
-			numVMOffsets.Read(reader, this, "numVMOffsets");
+			numVMOffsets.Read(reader, this, "numVMOffsets", SR1_File.Version.Jan23, SR1_File.Version.Next);
 
 			offsets = (SR1_StructureArray<VMColorOffset>)(new SR1_StructureArray<VMColorOffset>(numVMOffsets.Value)).SetPadding(4);
 			offsets.Read(reader, this, "offsets");
@@ -22,7 +31,7 @@ namespace Recombobulator.SR1Structures
 
 		public override void WriteMembers(SR1_Writer writer)
 		{
-			numVMOffsets.Write(writer);
+			numVMOffsets.Write(writer, SR1_File.Version.Jan23, SR1_File.Version.Next);
 			offsets.Write(writer);
 		}
 	}
